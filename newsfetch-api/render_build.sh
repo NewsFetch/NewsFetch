6fbdd1baafdba6f6ba6e4059ab839cc8cc78cc2b
Manoj Bharadwaj <mbharadwaj@gmail.com>
2022-09-18 16:32:32 -0400

    deploying API to Render

--- newsfetch-api/render_build.sh
+++ newsfetch-api/render_build.sh
@@ -1,3 +1,3 @@
-pip install poetry==1.1.12 &&  \
-    poetry export --without-hashes -o requirements.txt &&  \
-    pip install --no-cache-dir -r requirements.txt
+pip install poetry==1.1.12
+poetry export --without-hashes -o requirements.txt
+pip install --no-cache-dir -r requirements.txt
